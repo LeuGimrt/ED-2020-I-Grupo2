b6da870c9bc38c8582d2544d933a83abc8e4aac2
DAN <daercimi@gmail.com>
2020-07-14 20:09:38 -0500

    Funciones Extra de pila subido

--- Tarea 04 Pila y Cola Basica/PilaBasica/src/PilaBasica.c
+++ Tarea 04 Pila y Cola Basica/PilaBasica/src/PilaBasica.c
@@ -15,6 +15,54 @@ typedef enum { false, true } bool;
 
 bool flag=false;
 
+//Agregado por Cifuentes Michuy
+int empilar(int elemento){
+	if(laPila.tope >=100){
+		printf("Pila llena\n");
+		fflush(stdout);
+		return ERROR_PILA_LLENA;
+	}
+	else{
+		laPila.tope++;
+		laPila.datos[laPila.tope] = elemento;
+		return laPila.tope;
+	}
+}
+
+int desempilar(){
+	if(laPila.tope < 0){
+		return ERROR_PILA_VACIA;
+	}
+	else{
+		int elemento = laPila.datos[laPila.tope];
+		laPila.tope--;
+		return elemento;
+	}
+}
+
+void limpiar(){
+	while(laPila.tope>=0){
+		desempilar();
+	}
+}
+
+void mostrar(){
+	printf("valores de pila: ");
+	for(int i=0;i<=laPila.tope; i++){
+		printf("%4d, ",laPila.datos[i]);
+	}
+	printf("\n");
+	printf("posiciones:      ");
+	for(int i=0;i<=laPila.tope; i++){
+		printf("%4d, ",i);
+	}
+	printf("\n");
+
+	fflush(stdout);
+}
+//Agregado por Cifuentes Michuy
+
+
 int main(void) {
 
 
